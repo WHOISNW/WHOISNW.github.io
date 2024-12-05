f1710161d1aceca581a98229430f143ad174eb85
WHOISNW <natthew1@outlook.com>
2024-12-05 22:00:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener('click', () => {
+menuIcon.onclick = () => {
+   menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-    menuIcon.classList.toggle('active');
-});
+};
 
 
